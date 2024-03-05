f2e7a44915bc3d1e80b8b36fe2fd6b3f463982d3
byMarkky <mo6368745@gmail.com>
2024-03-05 12:58:06 +0100

    Programa basico

---
+++ src/Alumno/Alumno.java
@@ -0,0 +1,46 @@
+package Alumno;
+
+public class Alumno {
+    private String dni;
+    private String nombre;
+    private int edad;
+
+    public Alumno(String dni, String nombre, int edad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+}
